fae10d089f11c8fe65d247967e61451e31add72c
zahrayousefijamarani <45602698+zahrayousefijamarani@users.noreply.github.com>
2020-06-05 19:22:10 +0430

    add trie code and get input

---
+++ hw3.swift
@@ -0,0 +1,75 @@
+
+import Foundation
+
+struct Node {
+    var children : Array<Node?> = {
+       var returnArray =  Array<Node?>() ;
+       
+       for _ in 0...25 {
+         returnArray.append(nil)   
+       } 
+       
+       return returnArray;
+    }() 
+    
+    var endWord = false
+}
+
+class Trie {
+    var root = Node()
+    
+    func insert (key : String) {
+        var parent = root;
+        var index : Int;
+        for char in key {
+        
+            if char.asciiValue != nil {
+                index = Int((char.asciiValue)! - (Character("A").asciiValue)!) 
+            
+                if parent.children[index] == nil { // The character not found 
+                    parent.children[index] = Node()
+                }
+                parent = (parent.children[index])!
+            
+            }
+            else{
+                print("false input!")
+                return
+            }
+        }
+        parent.endWord = true ; // at the end of insertion the word will end
+    }
+    
+    func search (key : String) -> Bool {
+        var parent = root
+        var index : Int;
+        for char in key {
+        
+            if char.asciiValue != nil {
+                index = Int((char.asciiValue)! - (Character("A").asciiValue)!) 
+            
+                if parent.children[index] == nil { // The character not found 
+                    return false;
+                }
+                parent = (parent.children[index])!
+            
+            }
+            else{
+                print("false input!")
+                return false
+            }
+        }
+        return parent.endWord == true
+    }
+}
+
+var words = readLine()!.components(separatedBy: " ") ;
+
+var size = readLine()!.components(separatedBy: " ") ;
+
+M = Int(size[0])
+N = Int(size[1])
+
+for i in 0...M{
+    input = readLine();
+}
